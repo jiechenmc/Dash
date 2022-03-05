b10ddee4a3a6383f1a4685933c800b2674b81cef
Jie Chen <jiechenmc@gmail.com>
2022-03-04 20:52:18 -0500

    Made the time update in every second

--- src/widgets/TimeWidget.js
+++ src/widgets/TimeWidget.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function TimeWidget() {
   const parseTime = () => {
     const timeOfDay = date.getHours() - 12 < 0 ? "AM" : "PM";
     const hours = timeOfDay === "PM" ? date.getHours() - 12 : date.getHours();
     const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const seconds =
+      date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
     return `${hours}:${minutes}:${seconds} ${timeOfDay}`;
   };
 
-  const date = new Date();
+  const [date, setDate] = useState(new Date());
+
+  // This useEffect hook only runs once when the component mounts
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   const today = date.toDateString();
   const time = parseTime();
 

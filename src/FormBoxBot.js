112b56832d3ef2d04736e5899dd39ce562218487
Jie Chen <jiechenmc@gmail.com>
2022-03-04 23:52:15 -0500

    change location to dice

--- src/FormBoxBot.js
+++ src/FormBoxBot.js
@@ -1,13 +1,13 @@
 import React from "react";
 import AssignmentWidget from "./widgets/AssignmentWidget";
-import LocationWidget from "./widgets/LocationWidget";
+import DiceWidget from "./widgets/Dice";
 import SettingsWidget from "./widgets/SettingsWidget";
 
 export default function FormBoxBot() {
   return (
     <div id="FormBoxBot">
       <AssignmentWidget></AssignmentWidget>
-      <LocationWidget></LocationWidget>
+      <DiceWidget></DiceWidget>
       <SettingsWidget></SettingsWidget>
     </div>
   );

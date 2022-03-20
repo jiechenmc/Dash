7f16f76a1ad9695ceadbcde824612b5c2aebd47b
Jie Chen <jiechenmc@gmail.com>
2022-03-20 14:09:07 -0400

    -- Marks the beginning of site redesign --

---
+++ src/redesign/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeadBar from "./HeadBar";
+import Content from "./Content";
+import FootBar from "./FootBar";
+
+export default function HomeScreen() {
+  return (
+    <>
+      <HeadBar />
+      <Content />
+      <FootBar />
+    </>
+  );
+}

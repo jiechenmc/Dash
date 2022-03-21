58cbc516df60a6bf22f7fcd06f76057e5532020d
Jie Chen <jiechenmc@gmail.com>
2022-03-21 15:53:44 -0400

    preparing to add server back

---
+++ client/src/FootBar/Login.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function Login() {
+  return (
+    <div className="bg-green-500">
+      <button>Login with Spotify</button>
+    </div>
+  );
+}

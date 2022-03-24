a41f60676d6e631d166e0554f19be6d4a5a0280e
Jie Chen <jiechenmc@gmail.com>
2022-03-23 22:33:42 -0400

    changed weather compact view to use the props instead

--- client/src/HeadBar/WeatherViews/WeatherCompact.js
+++ client/src/HeadBar/WeatherViews/WeatherCompact.js
@@ -4,9 +4,9 @@ export default function WeatherCompact({ city, weather, temperature }) {
   return (
     <div className="flex flex-row gap-1">
       <i className="bi-cloud text-xl"></i>
-      <p className="text-xl">Stony Brook</p>
-      <p className="text-sm">Sunny</p>
-      <p className="text-sm">53.6°F|12°C</p>
+      <p className="text-xl">{city}</p>
+      <p className="text-sm">{weather}</p>
+      <p className="text-sm">{temperature}</p>
     </div>
   );
 }

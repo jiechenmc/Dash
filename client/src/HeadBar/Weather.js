a7bf7c9bb769d1d49921117d32cee0f1812d3e16
Jie Chen <jiechenmc@gmail.com>
2022-03-26 21:31:58 -0400

    some boilerplate for kaitlyn

--- client/src/HeadBar/Weather.js
+++ client/src/HeadBar/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import WeatherComfort from "./WeatherViews/WeatherComfort";
 import WeatherCompact from "./WeatherViews/WeatherCompact";
 
@@ -6,6 +6,8 @@ export default function Weather({ isCompact }) {
   const [city, setCity] = useState("Stony Brook");
   const [weather, setWeather] = useState("Sunny");
   const [temperature, setTemperature] = useState("53.6°F|12°C");
+  const inputElement = useRef(null);
+  const userInput = inputElement.current.value;
 
   const props_list = {
     city,
@@ -15,6 +17,7 @@ export default function Weather({ isCompact }) {
 
   return (
     <div>
+      <input ref={inputElement} className="bg-gray-600"></input>
       {isCompact ? (
         <WeatherCompact {...props_list} />
       ) : (

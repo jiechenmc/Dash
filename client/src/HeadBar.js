4d404d4e059d94334482c5c5048a796746d64551
Jie Chen <jiechenmc@gmail.com>
2022-04-16 22:02:14 -0400

    added message

--- client/src/HeadBar.js
+++ client/src/HeadBar.js
@@ -5,6 +5,8 @@ import Time from "./HeadBar/Time";
 export default function HeadBar() {
   return (
     <div className="inline-flex gap-2 bg-neutral-700 text-white text-mono">
+      {/* Temporary message*/}
+      <p className="absolute left-0 ">Currently in the works!</p>
       <Time />
       <Github />
     </div>
